d46821d7215d99590a50982aa0de580c2cd79e96
joaopcs80 <joaopcs80@gmail.com>
2024-09-20 16:58:07 +0000

    Sep 20, 2024, 1:58 PM

--- Projeto-Japa-01/src/MinhaClasse.java
+++ Projeto-Japa-01/src/MinhaClasse.java
@@ -3,10 +3,13 @@ public class MinhaClasse {
     final int ano = 2011;
 
   public static void main(String[] args) {
-    System.out.println("Olá time, sejam bem vindos");
-
-
-
+    System.out.println(somar(5, 6));
   }
+
+  public static int somar (int numero1, int numero2) {
+    return numero1 + numero2;
+  }
+
+  
 
 }
